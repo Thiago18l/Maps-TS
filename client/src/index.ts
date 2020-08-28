ab3b5a7105317f3c3c2f579e24fe1bca887c352c
Thiago Lopes <thiago.lopes.dev@gmail.com>
2020-08-28 00:27:03 -0300

    feat: add google maps support

--- client/src/index.ts
+++ client/src/index.ts
@@ -5,3 +5,11 @@ const user = new User()
 console.log(user)
 const company = new Company()
 console.log(company)
+
+new google.maps.Map(document.getElementById("map"), {
+    zoom: 1,
+    center: {
+        lat: 0,
+        lng: 0,
+    }
+});
