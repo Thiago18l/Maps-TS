d6831eb9a836e2125c0c6cc5927fca5eb89b57ac
Thiago Lopes <thiago.lopes.dev@gmail.com>
2020-08-30 10:21:32 -0300

    refactor: update content in interface

--- client/src/CustomMap.ts
+++ client/src/CustomMap.ts
@@ -4,6 +4,7 @@ interface IMappable {
         lat: number;
         lng: number;
     }
+    markerContent(): string;
 }
 export class CustomMap {
     private googleMap: google.maps.Map;
@@ -19,12 +20,18 @@ export class CustomMap {
     }
 
     addMarker(mappable: IMappable): void {
-        new google.maps.Marker({
+        const marker = new google.maps.Marker({
             map: this.googleMap,
             position: {
                 lat: mappable.location.lat,
                 lng: mappable.location.lng,
             }
+        })
+        marker.addListener('click', () => {
+            const infoWindow = new google.maps.InfoWindow({
+                content: 'Hi there!',
+            })
+            infoWindow.open(this.googleMap, marker);
         })
     }
 }
